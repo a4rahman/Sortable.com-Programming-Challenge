d22cdbda8d61fbc79dbebcb071ca16e8248be134
Asif Rahman <rahman903@gmail.com>
2012-04-05 19:18:19 -0400

    Removed unused import

--- productMatcher/src/com/sortable/productmatcher/matcher/ProductMatcherMain.java
+++ productMatcher/src/com/sortable/productmatcher/matcher/ProductMatcherMain.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
